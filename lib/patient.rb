1cee231fa4e9038ea1c1f7191f5151c3e4501929
Jack Goode <jack.w.goode@icloud.com>
2020-06-27 16:11:12 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -20,7 +20,7 @@ class Patient
   
   def appointments 
     Appointment.all.select do |appointment| 
-      appointment.patient == self 
+      appointment == self 
     end
     
     def doctors 
